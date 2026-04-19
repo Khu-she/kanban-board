edd2c22b1db644fb404c41a735d3ee38085128bb
Khushi Mogha <155957831+Khu-she@users.noreply.github.com>
2026-04-19 16:58:04 +0530

    adding server

---
+++ server/src/main/java/com/kanban/server/controllers/TaskController.java
@@ -0,0 +1,45 @@
+package com.kanban.server.controllers;
+
+import com.kanban.server.models.task.TaskDTO;
+import com.kanban.server.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
+@CrossOrigin(origins="http://localhost:3000")
+public class TaskController {
+    @Autowired
+    TaskService taskService;
+
+    @PostMapping("")
+    public TaskDTO addTask(@RequestBody TaskDTO task) {
+
+        return taskService.addTask(task);
+    }
+    @GetMapping("")
+    public ResponseEntity<List<TaskDTO>> getAllTasks(){
+        return new ResponseEntity<List<TaskDTO>>(taskService.getAllTasks(), HttpStatus.OK);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskDTO> updateTask(@PathVariable Long id,@RequestBody TaskDTO task){
+
+        return new ResponseEntity<TaskDTO>(taskService.updateTask(id,task),HttpStatus.OK);
+    }
+    @PatchMapping("/{id}")
+    @CrossOrigin(origins="http://localhost:3000")
+    public ResponseEntity<TaskDTO> updateTaskStatus(@PathVariable Long id,@RequestBody TaskDTO status){
+
+        return new ResponseEntity<TaskDTO>(taskService.updateTaskStatus(id,status.getStatus()),HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return new ResponseEntity<String>("Task Deleted",HttpStatus.OK);
+    }
+
+}
---
+++ server/src/main/java/com/kanban/server/services/TaskService.java
@@ -0,0 +1,54 @@
+package com.kanban.server.services;
+
+import com.kanban.server.models.task.TaskDAO;
+import com.kanban.server.models.task.TaskDTO;
+import com.kanban.server.repository.TaskRepository;
+import com.kanban.server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    TaskRepository taskRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    public TaskDTO addTask(TaskDTO task) {
+        TaskDAO taskDAO= taskRepository.save(TaskDAO.builder().title(task.getTitle()).description(task.getDescription()).status(task.getStatus()).assignedTo( task.getAssignedTo()).build());
+        return TaskDTO.builder().id(taskDAO.getId()).title(taskDAO.getTitle()).description(taskDAO.getDescription()).status(taskDAO.getStatus()).assignedTo(taskDAO.getAssignedTo()).build();
+    }
+    public List<TaskDTO> getAllTasks(){
+        List<TaskDTO> taskDTOS= new ArrayList <>();
+       taskRepository.findAll().forEach(taskDAO ->
+               taskDTOS.add(TaskDTO.builder().id(taskDAO.getId()).title(taskDAO.getTitle()).description(taskDAO.getDescription()).status(taskDAO.getStatus()).assignedTo( taskDAO.getAssignedTo()).build()
+               ));
+        return taskDTOS;
+    }
+    public TaskDTO updateTaskStatus(Long id,String status){
+        TaskDAO taskDAO=taskRepository.findById(id).get();
+        taskDAO.setStatus(status);
+        taskRepository.save(taskDAO);
+        return TaskDTO.builder().id(taskDAO.getId()).title(taskDAO.getTitle()).description(taskDAO.getDescription()).status(taskDAO.getStatus()).assignedTo( taskDAO.getAssignedTo()).build();
+    }
+    public TaskDTO updateTask(Long id,TaskDTO task){
+
+        TaskDAO taskDAO=taskRepository.findById(id).get();
+        taskDAO.setTitle(task.getTitle());
+        taskDAO.setDescription(task.getDescription());
+        taskDAO.setStatus(task.getStatus());
+        taskDAO.setAssignedTo(task.getAssignedTo());
+
+        taskRepository.save(taskDAO);
+        return TaskDTO.builder().id(taskDAO.getId ()).title(taskDAO.getTitle()).description(taskDAO.getDescription()).status(taskDAO.getStatus()).assignedTo( taskDAO.getAssignedTo()).build();
+    }
+    public void deleteTask(Long id){
+        taskRepository.deleteById(id);
+    }
+
+}
